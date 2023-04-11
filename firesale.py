3559f6c58ff566b9dd10d9cd5a62d9b18676242e
sam-acker <samuelacker@protonmail.com>
2023-04-11 17:49:52 -0600

    todos

--- firesale.py
+++ firesale.py
@@ -1,6 +1,10 @@
 import yaml, sys, requests, openai, time, pickle
 from bs4 import BeautifulSoup
+from alpaca.trading.client import TradingClient
 
+# TODO: document functions
+
+# FUNCTIONS
 def configure():
     with open('config.yaml', 'r') as f:
         data = yaml.safe_load(f)
@@ -9,6 +13,7 @@ def configure():
         data["configured"] = 1
 
         data["alpacaAPIKey"] = input("alpaca api key: ")
+        data["alpacaSecret"] = input("alpaca secret: ")
         data["openaiAPIKey"] = input("openai api key: ")
         data["value"] = int(input("Amount for put options: "))
 
@@ -63,6 +68,15 @@ def check():
     return check
 
 # TODO: make alpaca short sell function
+# https://alpaca.markets/docs/trading/getting_started/
+def short(ticker_input):
+    print("short placeholder")
+
+    API_KEY = config["alpacaAPIKey"]
+    SECRET_KEY = config["alpacaSecret"]
+
+    trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
+
 
 # SETUP
 with open('config.yaml', 'r') as file:
@@ -81,6 +95,8 @@ if configured == 0:
 link_chache = {""}
 ticker_cache = {""}
 
+# TODO: check .pkl files to see if they're empty
+
 with open('link_cache.pkl', 'rb') as fp:
     link_chache = pickle.load(fp)
 
@@ -93,7 +109,8 @@ if (len(sys.argv) == 2) and (sys.argv[1] == "start"):
     # scraped_text = scrape("http://hindenburgresearch.com/block/")
     # print(scraped_text)
     # print(get_ticker("test"))
-    print("___MAIN___")
+    # print("___MAIN___")
+    print(get_ticker("test"))
     loop = 0
     while loop < 4:
         # print("_____________")
@@ -105,12 +122,24 @@ if (len(sys.argv) == 2) and (sys.argv[1] == "start"):
             
             # TODO: pass article text to get_ticker()
 
+            # TODO: ticker cache
+
             # TODO: pass ticker to alpaca function
 
         # print("_______sleeping_______")
         loop += 1
         time.sleep(5)
 
+
+# TODO: make better exit system that still saves
+
+# TODO: make python style correct main function
+
+# TODO: better argument handling
+
+# TODO: .gitignore for yaml and yaml builder
+
+# TODO: proper application structure
 
 # SAVE
 with open('link_cache.pkl', 'wb') as fp:
